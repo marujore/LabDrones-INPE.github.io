2eb70c5b75f29cba46154c4070851189ab5adf83
marujo <rennanmarujo@gmail.com>
2026-09-22 14:28:49 -0300

    Update tile server

--- geocatalog/js/app.js
+++ geocatalog/js/app.js
@@ -279,9 +279,9 @@ window.onload = function() {
     // Inicializa o grupo de feições visíveis
     feicoesVisiveisGroup = L.featureGroup().addTo(map);
 
-    osmLayer = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    osmLayer = L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Street_Map/MapServer/tile/{z}/{y}/{x}', {
         maxZoom: 19,
-        attribution: '&copy; OpenStreetMap'
+        attribution: 'Tiles &copy; Esri &mdash; Esri, HERE, Garmin, © OpenStreetMap contributors, and the GIS user community'
     });
 
     esriImageryLayer = L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}', {
